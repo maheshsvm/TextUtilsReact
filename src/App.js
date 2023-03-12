b3641164706cf781c50244b0d69694b6d8f6c142
Mahesh Sharma <maheshkaushik04611@gmail.com>
2023-03-12 14:19:06 +0530

    Revert "Routes included"
    This reverts commit 13e6d34eafb6209fc4aa15b839284d1fd098b820.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,30 @@
+import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
-//rfce --> to import react function based component
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null);
-  const [darkModeColorHidden, setDarkModeColorHidden] = useState(true)
-
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode('dark');
-      document.body.style.backgroundColor = '#222528';
-      updateAlert("Routesed to dark mode", "success");
-      setDarkModeColorHidden(false);
-
-      // for blinking the title -- bad user experience
-      // setInterval(() => {
-      //   document.title = 'text utils is amazing'
-
-      // }, 200);
-
-      // setInterval(() => {
-      //   document.title = 'Install text utils now'
-      // }, 150);
-
-    } else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      updateAlert("Routesed to light mode", "success");
-      setDarkModeColorHidden(true);
-    }
-  }
-
-
-  let modeStyle = {
-    color: mode === 'dark' ? "white" : "black"
-  }
-
-  const updateAlert = (message, type) => {
-    setAlert({
-      message: message,
-      type: type
-    })
-    setTimeout(() => {
-      setAlert(null)
-    }, 2000);
-  }
-
   return (
     <>
+      <Router>
         <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} modeStyle={modeStyle} toggleMode={toggleMode} darkModeColorHidden={darkModeColorHidden} />
         <Alert alert={alert} updateAlert={updateAlert} />
         <div className="container my-4">
-            <TextForm heading="Enter your text below" mode={mode} modeStyle={modeStyle} updateAlert={updateAlert} />
-            {/* <About></About> */}
+          <Routes>
+            <Route path="/about" element = {<About />} />
+
+            <Route path="/" element = {<TextForm heading="Enter your text below" mode={mode} modeStyle={modeStyle} updateAlert={updateAlert} />} />
+          </Routes>
         </div>
+      </Router>
     </>
   );
 }
